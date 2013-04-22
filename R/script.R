25ce4106bf27bc745a1801561946d0227535e7eb
htorres <mnemonico@posthocergopropterhoc.net>
2013-04-22 01:47:53 -0300

    the R script now builds an almost complete dataframe with all tcga lgg data

--- R/script.R
+++ R/script.R
@@ -1,24 +1,40 @@
-#!/usr/bin/env Rscript
+#!/usr/bin/e nv Rscript
 library(stringr)
 
 #TODO: put this whole section into build.tcga.dataframe function with a 'folder' argument
+#the final dataframe should have 217 columns vs 450k rows
 
 TARs = list.files('../tcgawebcrawler/datafiles/', pattern='*.tar.gz$', full.names=TRUE)
 decompress = function(filepath) { system(paste('tar xzvf', filepath,  '-C ../tcgawebcrawler/datafiles/')) }
 sapply(TARs, decompress)
-getfolder = function(fname) { return(as.character(strsplit(fname, '.tar.gz')))} 
-folders = as.character(lapply(TARs, getfolder))
 
-build.batch.dataframe = function(folder) {
-    files = (list.files(folder, pattern='*TCGA*', full.names=TRUE) )
+build.batch.dataframe = function() {
+    #setwd('../tcgawebcrawler/datafiles')
+    pattern = '*TCGA-([0-9A-Z]{2})-([0-9A-Z]{4})-(0[0-9]|[1][0-9])([A-Z])-(0[0-9]|[1-9][0-9])([DGHRTWX])-([0-9A-Z]{4})-(\\d{2})*'
+    files = (list.files(pattern=pattern, full.names=TRUE, recursive=TRUE) )
     merged = data.frame() #each patient sample's methylation beta value will be added to this
     for (file in files) {
-        #a regular expression to extract the sample patient code
-        patient = str_extract(file, "TCGA-(\\d+)-(\\d+)-(\\d+[A-Z]+)-(\\d+[A-Z]+)-(\\d+)-(\\d+)")
-        dataframe = read.table(files, sep='\t', skip=1, header=TRUE)
-        #TODO: merge data frames
+        patient = str_extract(file, pattern)
+        if ( length(merged) == 0) {
+            #a regular expression to extract the sample patient code
+            dataframe = read.table(file, sep='\t', skip=1, header=TRUE)
+            colnames(dataframe)[2] = patient
+            merged=dataframe[,c(1,3,4,5,2)]
+        }
+        else {
+            dataframe = read.table(file, sep='\t', skip=1, header=TRUE)
+            colnames(dataframe)[2] = patient
+            merged = cbind(merged, dataframe[2])
+        }
     } 
+    #remover as linhas dos cromossomos X e Y.
+    return(merged)
 } 
+
+#construir um manifesto com id do paciente, status tumoral, níveis, batch
+#ver wiki do tcga barcode
+#calcular o desvio padrão dos beta values(adicionar no final)
+
 
 
 
